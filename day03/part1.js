f50aa236dbf248181ef32c2540b68a56e8728f96
Gwilym Johnston <gwilym.johnston@zoopla.co.uk>
2020-12-04 14:31:04 +0000

    prettier

--- day03/part1.js
+++ day03/part1.js
@@ -1,16 +1,11 @@
-const { data } = require('./data.js'); 
+const { data } = require("./data.js");
 
 const getRowInfo = (row, i) => {
-  return row.charAt(i*3%row.length);
+  return row.charAt((i * 3) % row.length);
 };
 
+const isTree = (step) => step === "#";
 
-const isTree = (step) => step === '#';
-
-const result = data
-                .map(getRowInfo)
-                .filter(isTree);
+const result = data.map(getRowInfo).filter(isTree);
 
 console.log(result.length);
-
-
--- day03/part2.js
+++ day03/part2.js
@@ -1,25 +1,21 @@
-
 // Part 2
 const { data } = require("./data.js");
 
 const getRowInfo = (shift) => (down) => (result, row, i) => {
   const char = (i * (shift / down)) % row.length;
   const item = row.charAt(char);
-    if(i%down === 0) {
-      result.push(item);
-    }
-    return result;
+  if (i % down === 0) {
+    result.push(item);
+  }
+  return result;
 };
 
-const getTotal = (acc, val) =>  acc * val.length
-
+const getTotal = (acc, val) => acc * val.length;
 
 const isTree = (down) => down === "#";
 
-const getResults = ({ shift, down }) => 
- data
-    .reduce(getRowInfo(shift)(down), [])
-    .filter(isTree)
+const getResults = ({ shift, down }) =>
+  data.reduce(getRowInfo(shift)(down), []).filter(isTree);
 
 const types = [
   { shift: 1, down: 1 },
@@ -28,7 +24,6 @@ const types = [
   { shift: 7, down: 1 },
   { shift: 1, down: 2 },
 ];
-
 
 const results = types.map(getResults).reduce(getTotal, 1);
 
